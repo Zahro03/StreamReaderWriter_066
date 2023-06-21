f9346736654517605a78db80d07e0bc26499cabe
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-06-21 10:57:52 +0700

    menampilkan 2 variabel string baris dan string Nama file

---
+++ iofileWithPath/iofileWithPath.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File:";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << "<= Menulis file. \'q\' untuk keluar" << endl;
+
+	//unlimitied loop untuk menulis
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam satu kelas
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+	}
+}
