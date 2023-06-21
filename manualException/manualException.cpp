28bbce89ab821658ddc5780183ebb35268cde2ee
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-06-21 11:24:33 +0700

    untuk melempar atau memberi tahu errornya

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -7,7 +7,7 @@ int main()
 		cout << "Selamat Belajar di prodi TI UMY" << endl;
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << " Pernyataan tidak akan dieksekusi" << endl;
-	}
+	} 
 	catch (int a) {
 		//blok ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
